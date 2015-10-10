479e58583f16d1b8dc4beabcfda4a7083862a38e
Yue Zhang <y.zhang@berkeley.edu>
2015-10-10 04:35:12 -0700

    putting in changes from friday night

---
+++ gen/com/mapstutorial/simplerouting/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mapstutorial.simplerouting;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/mapstutorial/simplerouting/RoutingActivity.java
@@ -0,0 +1,196 @@
+/******************************************************************
+ * Copyright (C) 2011-2015 HERE Global B.V.  All rights reserved. *
+ ******************************************************************/
+
+package com.mapstutorial.simplerouting;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.here.android.mpa.common.GeoBoundingBox;
+import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.MapFragment;
+import com.here.android.mpa.mapping.MapRoute;
+import com.here.android.mpa.routing.RouteManager;
+import com.here.android.mpa.routing.RouteOptions;
+import com.here.android.mpa.routing.RoutePlan;
+import com.here.android.mpa.routing.RouteResult;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+
+public class RoutingActivity extends Activity implements ConnectionCallbacks, OnConnectionFailedListener {
+    private static final String LOG_TAG = RoutingActivity.class.getSimpleName();
+
+    // map embedded in the map fragment
+    private Map map = null;
+
+    // map fragment embedded in this activity
+    private MapFragment mapFragment = null;
+
+    // TextView for displaying the current map scheme
+    private TextView textViewResult = null;
+
+    // MapRoute for this activity
+    private static MapRoute mapRoute = null;
+    
+    GoogleApiClient mGoogleApiClient;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Search for the map fragment to finish setup by calling init().
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapfragment);
+        mapFragment.init(new OnEngineInitListener() {
+            @Override
+            public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
+                if (error == OnEngineInitListener.Error.NONE) {
+                    // retrieve a reference of the map from the map fragment
+                    map = mapFragment.getMap();
+                    // Set the map center coordinate to the Vancouver region (no animation)
+                    map.setCenter(new GeoCoordinate(37.4292, -122.1381, 0.0), Map.Animation.NONE);
+                    // Set the map zoom level to the average between min and max (no animation)
+                    map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
+                } else {
+                    Log.e(LOG_TAG, "Cannot initialize MapFragment (" + error + ")");
+                }
+            }
+        });
+
+        textViewResult = (TextView) findViewById(R.id.title);
+        textViewResult.setText(R.string.textview_routecoordinates_2waypoints);
+        
+        buildGoogleApiClient();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    // Functionality for taps of the "Get Directions" button
+    public void getDirections(View view) {
+        // 1. clear previous results
+        textViewResult.setText("");
+        if (map != null && mapRoute != null) {
+            map.removeMapObject(mapRoute);
+            mapRoute = null;
+        }
+
+        // 2. Initialize RouteManager
+        RouteManager routeManager = new RouteManager();
+
+        // 3. Select routing options
+        RoutePlan routePlan = new RoutePlan();
+
+        RouteOptions routeOptions = new RouteOptions();
+        routeOptions.setTransportMode(RouteOptions.TransportMode.CAR);
+        routeOptions.setRouteType(RouteOptions.Type.FASTEST);
+        routePlan.setRouteOptions(routeOptions);
+
+        // 4. Select Waypoints for your routes
+        // START: Nokia, Burnaby
+        routePlan.addWaypoint(new GeoCoordinate(37.4292, -122.1381));
+
+        // END: Airport, YVR
+        routePlan.addWaypoint(new GeoCoordinate(37.8717, -122.2728));
+
+        // 5. Retrieve Routing information via RouteManagerEventListener
+        RouteManager.Error error = routeManager.calculateRoute(routePlan, routeManagerListener);
+        if (error != RouteManager.Error.NONE) {
+            Toast.makeText(getApplicationContext(),
+                    "Route calculation failed with: " + error.toString(), Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    private RouteManager.Listener routeManagerListener = new RouteManager.Listener() {
+        public void onCalculateRouteFinished(RouteManager.Error errorCode, List<RouteResult> result) {
+
+            if (errorCode == RouteManager.Error.NONE && result.get(0).getRoute() != null) {
+                // create a map route object and place it on the map
+                mapRoute = new MapRoute(result.get(0).getRoute());
+                map.addMapObject(mapRoute);
+
+                // Get the bounding box containing the route and zoom in (no animation)
+                GeoBoundingBox gbb = result.get(0).getRoute().getBoundingBox();
+                map.zoomTo(gbb, Map.Animation.NONE, Map.MOVE_PRESERVE_ORIENTATION);
+
+                textViewResult.setText(String.format("Route calculated with %d maneuvers.", result
+                        .get(0).getRoute().getManeuvers().size()));
+            } else {
+                textViewResult.setText(String.format("Route calculation failed: %s",
+                        errorCode.toString()));
+            }
+        }
+
+        public void onProgress(int percentage) {
+            textViewResult.setText(String.format("... %d percent done ...", percentage));
+        }
+    };
+    
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build();
+        
+        Log.e("", "Ayy lmao");
+    }
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+        	Log.e("RoutingActivity", String.valueOf(mLastLocation.getLatitude()) + String.valueOf(mLastLocation.getLongitude()));
+        	textViewResult.setText(String.valueOf(mLastLocation.getLatitude()) + String.valueOf(mLastLocation.getLongitude()));
+        } else {
+        	Log.e("", "Darn");
+        }
+	}
+
+	@Override
+	public void onConnectionSuspended(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        if (!mResolvingError) {  // more about this later
+            mGoogleApiClient.connect();
+//        }
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+}
